36a2d3af6ffdb0c69dea63514478a04da7fe6e61
kubadospial <kuba@dospial.com>
2021-05-04 17:18:43 +0200

    small refactor

--- projects/ngx-positioner/src/lib/application/behavior/behavior.service.ts
+++ projects/ngx-positioner/src/lib/application/behavior/behavior.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ScrollBehavior } from '../../models/scroll-behavior.enum';
+import {
+  ModuloIndicator,
+  ScrollBehavior,
+} from '../../models/scroll-behavior.enum';
 
 @Injectable()
 export class BehaviorService {
@@ -17,34 +20,91 @@ export class BehaviorService {
     behavior: ScrollBehavior,
     distance: number,
     moveSpeed: number
-  ): Promise<boolean> {
+  ): Promise<void> {
     return new Promise((resolve) => {
-      let i = this.scrollTop;
-      if (distance !== i) {
+      const currentPosition = this.scrollTop;
+      if (distance !== currentPosition) {
         if (behavior === ScrollBehavior.smooth) {
-          const loop = setInterval(() => {
-            if (distance < i) {
-              const modulo = (distance + i) % 10;
-              i -= modulo ? modulo : moveSpeed;
-              if (i <= distance) {
-                clearInterval(loop);
-                resolve(true);
-              }
-            } else {
-              const modulo = (distance - i) % 10;
-              i += modulo ? modulo : moveSpeed;
-              if (i >= distance) {
-                clearInterval(loop);
-                resolve(true);
-              }
-            }
-            this.scrollTop = i;
-          }, 0);
+          this._smoothScroll(distance, currentPosition, moveSpeed, resolve);
         } else {
           this.scrollTop = distance;
-          resolve(true);
+          resolve();
         }
       }
     });
+  }
+
+  private _smoothScroll(
+    distance: number,
+    currentPosition: number,
+    moveSpeed: number,
+    resolve: (value: void) => void
+  ) {
+    const loop = setInterval(() => {
+      if (distance < currentPosition) {
+        currentPosition = this._moveUp(
+          currentPosition,
+          distance,
+          moveSpeed,
+          loop,
+          resolve
+        );
+      } else {
+        currentPosition = this._moveDown(
+          currentPosition,
+          distance,
+          moveSpeed,
+          loop,
+          resolve
+        );
+      }
+      this.scrollTop = currentPosition;
+    }, 0);
+  }
+
+  private _moveUp(
+    i: number,
+    distance: number,
+    moveSpeed: number,
+    loop: any,
+    resolve: (value: void) => void
+  ): number {
+    const modulo = this._calcModulo(i, distance, ModuloIndicator.increment);
+    i -= modulo ? modulo : moveSpeed;
+    if (i <= distance) {
+      clearInterval(loop);
+      resolve();
+    }
+    return i;
+  }
+
+  private _moveDown(
+    i: number,
+    distance: number,
+    moveSpeed: number,
+    loop: any,
+    resolve: (value: void) => void
+  ): number {
+    const modulo = this._calcModulo(i, distance, ModuloIndicator.decrement);
+    i += modulo ? modulo : moveSpeed;
+    if (i >= distance) {
+      clearInterval(loop);
+      resolve();
+    }
+    return i;
+  }
+
+  private _calcModulo(
+    i: number,
+    distance: number,
+    indicator: ModuloIndicator
+  ): number {
+    const incrementedDist =
+      indicator === ModuloIndicator.increment
+        ? distance + i
+        : indicator === ModuloIndicator.decrement
+        ? distance - i
+        : 0;
+    return incrementedDist % 10;
   }
 }
--- projects/ngx-positioner/src/lib/models/scroll-behavior.enum.ts
+++ projects/ngx-positioner/src/lib/models/scroll-behavior.enum.ts
@@ -3,3 +3,8 @@ export const enum ScrollBehavior {
   smooth = 'smooth',
   auto = 'auto',
 }
+
+export const enum ModuloIndicator {
+  increment = 'increment',
+  decrement = 'decrement',
+}
